a4cb7c286e053f8b9d2d99c79c2da2a7ad3741b1
Paulo Vitor Juchimiuk <paulo.juchimiuk@redeancora.com.br>
2025-05-02 16:33:28 -0300

    feat: autenticação via Sanctum finalizada e listagem de contatos protegida implementada

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,9 +2,6 @@
 
 namespace App\Providers;
 
-use App\Domain\Repositories\ContactRepositoryInterface;
-use App\Infrastructure\Persistence\EloquentContactRepository;
-use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,7 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(ContactRepositoryInterface::class, EloquentContactRepository::class);
+        //
     }
 
     /**
@@ -22,8 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
-        });
+        //
     }
 }
